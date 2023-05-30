8b05aa0ea7378d80f91628c21f0ccd3886280f28
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-30 10:28:59 +0900

    webview内でcropper.jsによるトリミング操作を実現する

--- src/extension.ts
+++ src/extension.ts
@@ -8,11 +8,20 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("content-provider-sample.helloWorld", (args) => {
       // Create and show panel
-      const panel = vscode.window.createWebviewPanel("example.webview", "Hello World", vscode.ViewColumn.One, {});
+      const panel = vscode.window.createWebviewPanel("example.webview", "Hello World", vscode.ViewColumn.One, {
+        enableScripts: true
+      });
 
       // And get the special URI to use with the webview
       const imgSrc = panel.webview.asWebviewUri(vscode.Uri.file(args.path));
       const { width, height } = sizeOf(imgSrc.fsPath);
+
+      const cropperJs = panel.webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, "node_modules/cropperjs/dist/cropper.min.js")
+      );
+      const cropperCss = panel.webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, "node_modules/cropperjs/dist/cropper.min.css")
+      );
 
       panel.webview.html = `
         <!DOCTYPE html>
@@ -21,11 +30,32 @@ export function activate(context: vscode.ExtensionContext) {
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>WebView Example</title>
+          <link rel="stylesheet" href="${cropperCss}" />
         </head>
         <body>
           <div style="width: ${width}px; height: ${height}px">
             <img id="crop-target" width="${width}" height="${height}"src="${imgSrc}"/>
           </div>
+          <button id='crop-exec-button'>Crop</button>
+          <div>
+            <img id='preview' />
+          </div>
+          <div id="debug"></div>
+          <script src="${cropperJs}"></script>
+          <script>
+            const target = document.getElementById('crop-target');
+            const cropper = new Cropper(target, {
+              zoomable: false,
+            });
+            
+            const execButton = document.getElementById('crop-exec-button');
+            
+            execButton.addEventListener('click', () => {
+              const canvas = cropper.getCroppedCanvas();
+              const preview = document.getElementById('preview');
+              preview.src = canvas.toDataURL();
+            })
+          </script>
         </body>
         </html>
       `;
