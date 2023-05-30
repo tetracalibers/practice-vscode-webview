ecb942c2f2060fefada92654b5265e687e2f5ddb
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-30 09:21:59 +0900

    メニュー右クリックでWebViewを起動

--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,26 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// これから実装するので、この時点ではまだファイルが無い事に注意！
-import { WebViewProvider } from "./webview-provider";
-
 export function activate(context: vscode.ExtensionContext) {
   // WebView を登録
   context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider(
-      "example.webview", // package.json で設定した`id`と同じ値にして下さい
-      new WebViewProvider(context.extensionUri)
-    )
+    vscode.commands.registerCommand("content-provider-sample.helloWorld", () => {
+      // Create and show panel
+      const panel = vscode.window.createWebviewPanel("example.webview", "Hello World", vscode.ViewColumn.One, {});
+      panel.webview.html = `
+        <!DOCTYPE html>
+        <html lang="ja">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>WebView Example</title>
+        </head>
+        <body>
+          <h1>Hello World!</h1>
+        </body>
+        </html>
+      `;
+    })
   );
 }
 
